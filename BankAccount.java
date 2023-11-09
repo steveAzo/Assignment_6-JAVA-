637d3ea8cdbb0f6320496716bfb0151aebb268ae
Stephen Kwaku Azongo <135361078+steveAzo@users.noreply.github.com>
2023-11-09 22:48:48 +0000

    dangerous

--- BankAccount.java
+++ BankAccount.java
@@ -2,3 +2,23 @@ abstract class BankAccount {
     public abstract long deposit();
     public abstract long withdrawal();
 }
+
+public class SavingsAccount extends BankAccount{
+
+}
+
+public class CheckingAccount extends BankAccount {
+    //these are suggested attributes of this class from chat gpt, so implement them yourself
+    private int accountNumber;
+    private String accountHolderName;
+    private double accountBalance;
+    private boolean overdraftProtectionEnabled;
+    private ArrayList<Transaction> transactionHistory;
+}
+
+public class Customer {
+    //methods to display account balance
+    //methods todeposit money
+    //methods to withraw money
+}
+
